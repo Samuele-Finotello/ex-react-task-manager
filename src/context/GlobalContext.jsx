bcec69fd7423cd400bd6350833c4a98ac928df29
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-01-03 16:12:37 +0100

    [add] feat end point 3 milestone 3

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,11 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
 
+  const [tasks, setTasks] = useState([])
+
   return (
-    <GlobalContext.Provider value=''>
+    // così abbiamo accessibile la value a qualsiasi componente che si trova all'interno del provider
+    <GlobalContext.Provider value={{ tasks, setTasks }}>
       {children}
     </GlobalContext.Provider>
   )
