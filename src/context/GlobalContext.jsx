3f166bad2d0ce121f226039ce6d5442f0f202869
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-01-03 16:32:00 +0100

    [add] feat end point 4 milestone 2

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,10 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+const { API_URL } = import.meta.env
 
 export const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
 
   const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}/tasks`)
+      .then(res => res.json())
+      .then(data => setTasks(data))
+      .catch(error => console.error(error))
+  }, [])
 
   return (
     // così abbiamo accessibile la value a qualsiasi componente che si trova all'interno del provider
