cfccaf8f269aa38a7296eedc46b9b8640b16599b
Samuele-Finotello <samuelefinotello.147@gmail.com>
2026-01-07 16:11:47 +0100

    [add] feat end point 2 milestone 7

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom"
 import AddTask from "./pages/AddTask"
 import TaskList from "./pages/TaskList"
 import HomePage from "./pages/HomePage"
+import TaskDetail from "./pages/TaskDetail"
 import { GlobalProvider } from "./context/GlobalContext"
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/list" element={<TaskList />} />
           <Route path="/add" element={<AddTask />} />
+          <Route path="/task/:id" element={<TaskDetail />} />
         </Routes>
       </BrowserRouter>
     </GlobalProvider>
